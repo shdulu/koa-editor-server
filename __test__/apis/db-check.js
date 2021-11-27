b61cfd610d14cec6e00ca9ea1aa467730a4ae7ac
shdulu <17625329975>
2021-11-27 12:09:50 +0800

    chore: jest回调超时处理

--- __test__/apis/db-check.js
+++ __test__/apis/db-check.js
@@ -4,11 +4,12 @@
 
 const { get } = require('./_server')
 
-test('数据库连接', async () => {
+test('数据库连接', async done => {
     const { data, errno } = await get('/api/db-check')
+    done()
     const { redisConn, mysqlConn, mongodbConn } = data
     expect(errno).toBe(0)
     expect(redisConn).toBe(true)
     expect(mysqlConn).toBe(true)
     expect(mongodbConn).toBe(true)
-})
+}, 10000)
